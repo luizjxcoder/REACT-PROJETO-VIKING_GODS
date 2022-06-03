b3615233e7d128c34797f8e71c6a4bad4e90ad0a
jxcoder <luiz.jun19@gmail.com>
2022-06-02 21:43:01 -0300

    header

--- vikinggods/src/App.js
+++ vikinggods/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
-import Api from './api/Api';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
 const App = () => {
    return (
-   <div>
-      <Api />
+      //ROTAS TELAS HOME E LOGIN
+      <div>
+         <BrowserRouter>
+            <Header />
+            <Routes>
+               <Route path="/" element={<Home/>}/>
+               <Route path="/login" element={<Login/>}/>
+            </Routes>
+            <Footer />
+            </BrowserRouter>
       </div>
    );
 }
