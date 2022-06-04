356ca9af07026c120ac0385a7228977af221fc60
jxcoder <luiz.jun19@gmail.com>
2022-06-04 20:09:29 -0300

    apiform

--- vikinggods/src/Components/Login/LoginForm.js
+++ vikinggods/src/Components/Login/LoginForm.js
@@ -1,38 +1,42 @@
+//                                         //
+// FORMULÁRIO DE LOGIN //
+//                                         //
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import { TOKEN_POST } from '../../api';
 
 const LoginForm = () => {
-   const [username, setUsername] = React.useState('');
-   const [password, setPassword] = React.useState('');
 
-   function handleSubmit (event) {
+   const username = useForm();
+   const password = useForm();
+
+   async function handleSubmit(event) {
       event.preventDefault();
-      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-         method: 'Post',
-         headers: {
-            'Content-Type': 'application/json'
-         },
-         body: JSON.stringify({ username, password }),
-         
-      }).then(response => {
-         console.log(response);
-         return response.json();
-      }).then(json => {
-         console.log(json);
-      })
+
+      if (username.validate() && password.validate()) {
+
+         const { url, options } = TOKEN_POST({
+            username: username.value,
+            password: password.value,
+         });
+   
+         const response = await fetch(url, options); 
+         const json = await response.json(); 
+            
+         window.localStorage.setItem('token', json.token);
+        
+      }
    }
   return (
      <section>
         <h1>Login</h1>
         <form action="" onSubmit={handleSubmit}>
-           <input type="text"
-              value={username}
-              onChange={({ target }) => setUsername(target.value)} />
-           
-           <input type="text"
-              value={password}
-              onChange={({ target }) => setPassword(target.value)} />
-           <button>Entrar</button>
+           <Input labe="Usuário" type="text" name="username" {...username}/>
+           <Input labe="Senha" type="password" name="password" {...password}/>
+           <Button>Entrar</Button>
         </form>
      <Link to="/login/criar">Cadastro</Link>
      </section>
