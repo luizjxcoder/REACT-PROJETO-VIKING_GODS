efb7124cad596ac45f2fa0664abbc49655914ca4
jxcoder <luiz.jun19@gmail.com>
2022-06-07 21:49:35 -0300

    loginform2

--- vikinggods/src/Components/Header.js
+++ vikinggods/src/Components/Header.js
@@ -1,30 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 import { ReactComponent as Dogs } from '../Assets/dogs.svg';
-import {UserContext} from '../UserContext'
+import { UserContext } from '../UserContext';
 
 const Header = () => {
-   const { data } = React.useContext(UserContext);
-   
+  const { data } = React.useContext(UserContext);
+
   return (
-     <header className={styles.header}>
-        <nav className={`${styles.nav} container`}>
-           <Link className={ styles.logo } to="/" aria-label="Dogs - Home">
-              <Dogs/>
-           </Link>
-           {data ? (
-              <Link className={styles.login} to="/conta">
-                 {data.nome}
-              </Link>
-           ) : (
-              <Link className={styles.login} to="/login">
-                 Login / Criar
-              </Link>
-            )}
-        </nav>
-      </header>
-    
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
+      </nav>
+    </header>
   );
 };
 
