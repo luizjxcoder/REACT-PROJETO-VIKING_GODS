ee44c2058efeeb421eb5ea870ed80cdf0c8d7b3f
jxcoder <luiz.jun19@gmail.com>
2022-06-17 21:56:10 -0300

    .

--- vikinggods/src/Components/User/UserPhotoPost.js
+++ vikinggods/src/Components/User/UserPhotoPost.js
@@ -7,7 +7,7 @@ import Button from '../Forms/Button';
 import Error from '../Helper/Error';
 import { PHOTO_POST } from '../../api';
 import { useNavigate } from 'react-router-dom';
-//import Head from '../Helper/Head';
+//TODO import Head from '../Helper/Head';
 
 const UserPhotoPost = () => {
   const nome = useForm();
@@ -44,7 +44,7 @@ const UserPhotoPost = () => {
 
   return (
     <section className={`${styles.photoPost} animeLeft`}>
-      {/* <Head title="Poste sua foto" /> */}
+      {/* FIXME<Head title="Poste sua foto" /> */}
       <form onSubmit={handleSubmit}>
         <Input label="Nome" type="text" name="nome" {...nome} />
         <Input label="God" type="text" name="god" {...god} />
