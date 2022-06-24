11a5fd83b0ff946c196f29c2c6abe51466180c33
jxcoder <luiz.jun19@gmail.com>
2022-06-24 20:46:32 -0300

    svg animation

--- vikinggods/src/Components/Home.js
+++ vikinggods/src/Components/Home.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import Feed from './Feed/Feed';
+import Loading from './Helper/Loading';
 //import Head from './Helper/Head';
 
 const Home = () => {
   return (
     <section className="container mainContainer">
-      <div
-        title="Fotos"
-        description="Home do site Viking Gods, com o feed de fotos."
-      />
-      <div />
+    <Loading/>
     </section>
   );
 };
