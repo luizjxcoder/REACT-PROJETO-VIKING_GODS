83231ebd4c03b92e19fb0f6582b835b1b7179e88
jxcoder <luiz.jun19@gmail.com>
2022-06-27 00:03:48 -0300

    estilização

--- vikinggods/src/Components/Header.js
+++ vikinggods/src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
-import { ReactComponent as Gods } from '../Assets/dogs.svg';
+import { ReactComponent as Logo } from '../Assets/title.svg';
 import { UserContext } from '../UserContext';
 
 const Header = () => {
@@ -10,9 +10,10 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link className={styles.logo} to="/" aria-label="Gods - Home">
-          <Gods />
-        </Link>
+           <Link className={styles.logo} to="/" aria-label="Logo - Home">
+              <Logo />
+           </Link>           
+         
         {data ? (
           <Link className={styles.login} to="/conta">
             {data.nome}
